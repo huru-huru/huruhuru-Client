56825f195adac68a2c89491f6ce0aef71a146984
gominzip <ahaminji@naver.com>
2023-11-13 22:38:15 +0900

    chore : cicd test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 	icons: {
 		icon: '/img/testIcon.png',
 	},
-	keywords: ['탕후루', '2000년대', '추억', '테스트', '과일탕후루', '왕가탕후루','후루후루','추억테스트','심리테스트'],
+	keywords: ['탕후루', '2000년대', '추억', '테스트', '과일탕후루', '왕가탕후루','후루후루','추억테스트','심리테스트','mbti'],
 	openGraph: {
 		title: '탕후루 테스트',
 		description: '탕후루로 보는 2000년대 공감 테스트',
--- src/components/FontTest.tsx
+++ src/components/FontTest.tsx
@@ -1,14 +1,16 @@
 'use client';
+import Image from 'next/image';
 import React from 'react';
 import styled from 'styled-components';
 
 const FontTest = () => {
 	return (
 		<Wrapper>
-            🍊폰트테스트🍓
-			<Skybori>하늘보리</Skybori>
-            <DNF>던파2</DNF>
-            <Neo>둥근고딕</Neo>
+            <Image src="/img/testIcon.png" alt="" width={50} height={50} />
+			<Skybori>🍑 팀 왕가탕후루 🍒</Skybori>
+			<DNF>위대한 탕후루 테스트</DNF>
+			<OverlappingDNF>위대한 탕후루 테스트</OverlappingDNF>
+			<Neo>아자아자 파이팅</Neo>
 		</Wrapper>
 	);
 };
@@ -16,23 +18,44 @@ const FontTest = () => {
 export default FontTest;
 
 const Wrapper = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
-    background: linear-gradient(180deg, #7DEFE5 0%, #FAF4D4 100%);
-    font-size: 2rem;
-`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	min-height: 100vh;
+	background: linear-gradient(180deg, #7defe5 0%, #faf4d4 100%);
+`;
+
 const Skybori = styled.div`
 	font-family: 'SKYBORI';
+    color: #5b5b5b;
+    font-size: 1.5rem;
 `;
 
 const DNF = styled.div`
 	font-family: 'DNF Bit Bit v2';
+	font-size: 4rem;
+	background: linear-gradient(180deg, #fff 0%, #81efe5 100%);
+	background-clip: text;
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent;
+	z-index: 1;
+	filter: drop-shadow(1px 1px 1px black); /* Shadow effect */
+`;
+
+const OverlappingDNF = styled.div`
+	font-family: 'DNF Bit Bit v2';
+	font-size: 4rem;
+	font-weight: 1000;
+	background-clip: text;
+	position: relative;
+	color: #3b3b3b; /* Black color */
+	margin-top: -5.5rem; /* Adjust this value based on your design */
 `;
 
 const Neo = styled.div`
-    font-family: "NeoDunggeunmo Pro";
-`
+	padding-top: 1rem;
+    font-size: 2rem;
+	font-family: 'NeoDunggeunmo Pro';
+`;
