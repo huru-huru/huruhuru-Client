71d7c129b535a066ae7b3cd6ea1dd4abf401fe92
gominzip <ahaminji@naver.com>
2023-11-14 15:58:26 +0900

    chore : 공통 버튼 컴포넌트 옵션 추가

--- src/components/common/SelectButton.tsx
+++ src/components/common/SelectButton.tsx
@@ -2,43 +2,48 @@
 import React from 'react';
 import styled from 'styled-components';
 
-type SelectButtontPropsType = {
-	text: string;
-	bgColor: string;
+type SelectButtonPropsType = {
+  text: string;
+  bgColor: string;
+  size?: number; // 선택사항
 };
 
-const SelectButton = (props: SelectButtontPropsType) => {
-	return (
-		<>
-			<BtnBox $bgColor={props.bgColor}>{props.text}</BtnBox>
-			<ShadowBox />
-		</>
-	);
+const SelectButton = (props: SelectButtonPropsType) => {
+  return (
+    <>
+      <BtnBox $bgColor={props.bgColor} $size={props.size || 2}>
+        {props.text}
+      </BtnBox>
+      <ShadowBox />
+    </>
+  );
 };
 
 export default SelectButton;
 
-const BtnBox = styled.div<{ $bgColor: string }>`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	width: 80%;
-	height: 5.2rem;
-	border-radius: 35.625px;
-	border: 1.187px solid #646464;
-	background: ${(props) => props.$bgColor};
-	color: #464646;
-	font-family: 'DNF Bit Bit v2';
-	line-height: normal;
-	font-size: 2rem;
-    z-index: 1;
+const BtnBox = styled.div<{ $bgColor: string; $size: number }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 80%;
+  height: 5.2rem;
+  border-radius: 35.625px;
+  border: 1.187px solid #646464;
+  background: ${(props) => props.$bgColor};
+  color: #464646;
+  font-family: 'DNF Bit Bit v2';
+  line-height: normal;
+  font-size: ${(props) => props.$size}rem;
+  z-index: 1;
+  cursor: pointer;
 `;
 
 const ShadowBox = styled.div`
-	position: relative;
-	width: 80%;
-	height: 5.2rem;
-	border-radius: 35.625px;
-	background: #646464;
-    margin-top: -4.7rem;
+  position: relative;
+  width: 80%;
+  height: 5.2rem;
+  border-radius: 35.625px;
+  background: #646464;
+  margin-top: -4.7rem;
+  cursor: pointer;
 `;
