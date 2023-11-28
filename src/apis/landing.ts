54204444f309e10c08792ba71f3b416a2b423b96
CJY <cjy3458@naver.com>
2023-11-28 23:04:37 +0900

    [FEAT] 참여자 수 랜딩페이지 데이터 불러오기

---
+++ src/apis/landing.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const baseURL = `https://huruhuru.kro.kr/`;
+
+export const participants = async () => {
+	const result = await axios.get(`${baseURL}`);
+	console.log(result);
+	return result.data;
+};
