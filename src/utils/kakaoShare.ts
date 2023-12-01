53ddcef28446f40137c314fce0450ae1803a16dc
gominzip <ahaminji@naver.com>
2023-12-02 03:38:50 +0900

    fix : 배포환경 공유하기 안됨 문제 테스트

--- src/utils/kakaoShare.ts
+++ src/utils/kakaoShare.ts
@@ -1,51 +1,70 @@
 export function initKakao() {
-	// 선언되지 않았을 때만 실행하도록 if문으로 체크
-	if (window.Kakao) {
-		if (!window.Kakao.isInitialized()) {
-			window.Kakao.init(process.env.NEXT_PUBLIC_API_KEY);
-		}
-	}
+    // 선언되지 않았을 때만 실행하도록 if문으로 체크
+    if (window.Kakao) {
+        try {
+            if (!window.Kakao.isInitialized()) {
+                console.log('api 키 세팅완료');
+                window.Kakao.init(process.env.NEXT_PUBLIC_API_KEY);
+            }
+        } catch (error) {
+            console.error('Error initializing Kakao:', error);
+        }
+    }
 }
 
 export async function onClickShareKakaoTalk() {
-	if (window.Kakao) {
-		if (window.Kakao.Share) {
-			await window.Kakao.Share.sendScrap({
-				requestUrl: 'https://huruhuru.netlify.app/', // 페이지 url
-				templateId: 101453, // 메시지템플릿 번호
-			});
-		}
-	}
+    console.log('공유하기 버튼 실행');
+    if (window.Kakao) {
+        try {
+            if (window.Kakao.Share) {
+                await window.Kakao.Share.sendScrap({
+                    requestUrl: 'https://huruhuru.netlify.app/', // 페이지 url
+                    templateId: 101453, // 메시지템플릿 번호
+                });
+            }
+        } catch (error) {
+            console.error('Error sharing on KakaoTalk:', error);
+        }
+    }
 }
 
 
-export async function onClickShareResult1KakaoTalk(title:string, content:string) {
-	if (window.Kakao) {
-		if (window.Kakao.Share) {
-			await window.Kakao.Share.sendScrap({
-				requestUrl: 'https://huruhuru.netlify.app/', // 페이지 url
-				templateId: 101455, // 메시지템플릿 번호
-                templateArgs: {
-                    TITLE: `🍊 나의 탕후루는... ${title}탕후루!`, // 제목 텍스트
-                    DESC: content, // 설명 텍스트
-                    BTN: '추억여행하러 가기'
-                  },
-			});
-		}
-	}
+export async function onClickShareResult1KakaoTalk(title: string, content: string) {
+    if (window.Kakao) {
+        try {
+            if (window.Kakao.Share) {
+                await window.Kakao.Share.sendScrap({
+                    requestUrl: 'https://huruhuru.netlify.app/', // 페이지 url
+                    templateId: 101455, // 메시지템플릿 번호
+                    templateArgs: {
+                        TITLE: `🍊 나의 탕후루는... ${title}탕후루!`, // 제목 텍스트
+                        DESC: content, // 설명 텍스트
+                        BTN: '추억여행하러 가기',
+                    },
+                });
+            }
+        } catch (error) {
+            console.error('Error sharing result 1 on KakaoTalk:', error);
+        }
+    }
 }
-export async function onClickShareResult2KakaoTalk(title:string, content:string) {
-	if (window.Kakao) {
-		if (window.Kakao.Share) {
-			await window.Kakao.Share.sendScrap({
-				requestUrl: 'https://huruhuru.netlify.app/', // 페이지 url
-				templateId: 101455, // 메시지템플릿 번호
-                templateArgs: {
-                    TITLE: title, // 제목 텍스트
-                    DESC: content, // 설명 텍스트
-                    BTN: '기록 깨러가기'
-                  },
-			});
-		}
-	}
+
+export async function onClickShareResult2KakaoTalk(title: string, content: string) {
+    if (window.Kakao) {
+        try {
+            if (window.Kakao.Share) {
+                await window.Kakao.Share.sendScrap({
+                    requestUrl: 'https://huruhuru.netlify.app/', // 페이지 url
+                    templateId: 101455, // 메시지템플릿 번호
+                    templateArgs: {
+                        TITLE: title, // 제목 텍스트
+                        DESC: content, // 설명 텍스트
+                        BTN: '기록 깨러가기',
+                    },
+                });
+            }
+        } catch (error) {
+            console.error('Error sharing result 2 on KakaoTalk:', error);
+        }
+    }
 }
